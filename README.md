35a87c198ad0d135f27610246d324ab73e4c9265
Silke Maes <silke.maes@kuleuven.be>
2024-07-08 14:33:07 +0200

    Update readme & first benchmark. Radiation doesn't work correctly YET

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # krome-CSE
-The chemistry code KROME is made compatible with our AGB circumstellar envelope (CSE) chemistry.
+The chemistry code KROME is *in the process of* being made compatible with our AGB circumstellar envelope (CSE) chemistry.
+
+08/07/'24 update:
+    KROME runs, but the radiation component for the photodissociation reactions does not work yet. --> WORK IN PROGRESS
+
+### How to run?
+1. Run the bash script "build_UMIST.sh"
+2. Run the bash script "make_CSEkrome.sh"
+3. Run the bash script "run_CSE_krome.sh" with a inputfile as argument
--- save_krome_out.py
+++ save_krome_out.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 
 
-step = sys.argv[1]
+step = int(sys.argv[1])
 print(step)
 file = 'krome/build/KROME_ERROR_REPORT'
 
@@ -10,7 +10,10 @@ file = 'krome/build/KROME_ERROR_REPORT'
 specs,output = np.loadtxt(file, skiprows=6, usecols=(1,2), max_rows=468, dtype='str', unpack=True)
 
 
-outfile = 'out/'+step+'.out'
+# outfile = 'out/'+step+'.out'
+
+outfile = '/STER/silkem/fortran_0D/rates/20211015_gridC_Mdot1e-6_v17-5_T_eps_model_2022-12-24h17-06-51/krome/abs_'+str(step+1)+'.txt'
+
 with open(outfile, 'w') as f:
     for i in range(0,len(specs)):
-        f.write(f'{specs[i]} {output[i]}\n')
+        f.write(f'{i+1:>5} {specs[i]:<15} {output[i]}\n')
