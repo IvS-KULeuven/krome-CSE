c476565d73c8f549e4416cc72c3d9fa92427664f
Mats Esseldeurs <matsesseldeurs@yahoo.com>
2025-09-11 16:04:51 +0200

    update umist2krome to include IP and AP

--- build_UMIST.sh
+++ build_UMIST.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 echo "---------------------------------------------"
 
-arg=$1
 
-# This script is used to build the krome code compatible with the UMIST rate16 chemical network.
+# Parse main network argument and additional options
+arg=$1
+shift
+extra_args="$@"
+
+# This script is used to build the krome code compatible with the UMIST rate16 and rate22 chemical network.
 
 # Copy the UMIST rate16 network to the krome tools directory
 cp -r umist2krome_custm.py krome/tools/
 
-if [ -z $arg ]
+if [ -z "$arg" ]
 then
     echo "No chemical network specified as bash argument."
     echo "Please specify the network." 
     echo "Example: ./build_UMIST.sh umist_rate16"
+    echo "Example: ./build_UMIST.sh umist_rate22"
+    echo "You can also add options: ./build_UMIST.sh umist_rate16 -IP"
+    echo "Or: ./build_UMIST.sh umist_rate22 -AP"
     echo "---------------------------------------------"
     exit 1
 
@@ -21,10 +28,27 @@ fi
 if [ $arg == 'umist_rate16' ]; then
     echo "umist rate16 network selected."
     cp -r umist_rate16.rates krome/tools/
+elif [ $arg == 'umist_rate22' ]; then
+    echo "umist rate22 network selected."
+    cp -r ../rate22_cse_code/rate22_final.rates krome/tools/umist_rate22.rates
+    if [ -z "$extra_args" ]; then
+        echo "No extra arguments provided."
+    else
+        echo "Extra arguments provided: $extra_args"
+        if [[ "$extra_args" == *"-AP"* ]]; then
+            ap_value=$(echo "$extra_args" | grep -oP '(?<=-AP=)[^ ]*')
+            echo "AP option detected. Activating accretion photons for a companion with $ap_value K."
+            [ -n "$ap_value" ] && cp -r ../rate22_cse_code/AP_"$ap_value"K.rates krome/tools/
+        fi
+        if [[ "$extra_args" == *"-IP"* ]]; then
+            echo "IP option detected. Deactivating all photoreactions."
+            cp -r ../rate22_cse_code/IP.rates krome/tools/
+        fi
+    fi
 else
     echo $arg "is not a present chemical network."
+    exit 1
 fi
-
 
 # Navigate to correct krome directory
 cd krome/tools
@@ -32,8 +56,8 @@ cd krome/tools
 # Build the UMIST rate16 network in KROME format
 pwd
 echo ">> Building the UMIST rate16 network in KROME format."
-python umist2krome_custm.py $arg
-
+python umist2krome_custm.py $arg $extra_args
+exit 1
 # Add shielding to the H2 reaction (deactivate the H2 photodissociation reaction)
 sed -i '/,H2,,H,H,,,.*user_xi/s/$/ * 0/' network_umist.dat
 
@@ -48,5 +72,6 @@ echo ""
 
 # Load the krome module
 cd ../
-python krome -n networks/network_umist.dat -iRHS -noSinkCheck -noRecCheck -noTlimits -unsafe -skipODEthermo -skipJacobian
+# Pass extra_args to krome if present
+python krome -n networks/network_umist.dat -iRHS -noSinkCheck -noRecCheck -noTlimits -unsafe -skipODEthermo -skipJacobian $extra_args
 # -shielding R14
--- umist2krome_custm.py
+++ umist2krome_custm.py
@@ -1,33 +1,40 @@
 import sys
 
 # read UMIST 2022 rates and convert them to KROME format
-
-# print(sys.argv)
+IP = False # Internal photons
+AP = False # Accretion photons of a companion
 
 network = sys.argv[1]
+if len(sys.argv) > 2:
+    for arg in sys.argv[2:]:
+        if "-IP" in arg:
+            IP = True
+        if "-AP" in arg:
+            AP = int(arg[4:])
 
-fname_umist = network+'.rates' 
+fname_umist = network+'.rates'
 fname = "network_umist.dat"  # output file
-
 
 skip = ["PHOTON", "CRPHOT", "CRP"]
 body = "@format:idx,R,R,P,P,P,P,tmin,tmax,rate\n"
-body += "@common:user_Auv,user_alb,user_xi,user_AuvAv\n"
+body += "@common:user_Auv,user_alb,user_xi,user_AuvAv"
+if IP or AP: body += ",user_rscale"
+if IP: body += ",user_Gstar,user_Auv_star"
+if AP: body += ",user_Gcomp,user_Auv_comp"
+body += "\n"
 count = 0
 for row in open(fname_umist):
     srow = row.strip()
-    if srow == "" or srow.startswith("#"):
-        continue
+    if srow == "" or srow.startswith("#"): continue
     arow = srow.split(":")
     rtype = arow[1]
     rr = arow[2:4]
     pp = arow[4:8]
     ka, kb, kc = [float(x) for x in arow[9:12]]
     tmin, tmax = [float(x) for x in arow[12:14]]
-    # print(rr, pp, ka, kb, kc)
     rate = None
     if rtype == "CR":
-        rate = "%.2e *  (Tgas / 3.0e2)**(%.2f) * (1./(1.-user_alb)) * (%.2f)" % (ka, kb, kc)
+        rate = "%.2e * (Tgas / 3.0e2)**(%.2f) * (1./(1.-user_alb)) * (%.2f)" % (ka, kb, kc)
     elif rtype == 'CP':
         rate = "%.2e " % ka
     elif rtype == "PH":
@@ -39,8 +46,47 @@ for row in open(fname_umist):
         if kc != 0e0:
             rate += " * exp(-%.2f / Tgas)" % kc
 
-    body += "%d,%s,%s,%.2e,%.2e,%s\n" % (count, ",".join(rr), ",".join(pp), tmin, tmax, rate)
+    body += f"{count},{','.join(rr)},{','.join(pp)},{tmin:.2e},{tmax:.2e},{rate}\n"
     count += 1
+
+if IP:
+    for row in open("IP.rates"):
+        srow = row.strip()
+        if srow == "" or srow.startswith("#"): continue
+        arow = srow.split(":")
+        rtype = arow[1]
+        rr = arow[2:4]
+        pp = arow[4:8]
+        ka, kb, kc = [float(x.replace(',', '.')) for x in arow[9:12]]
+        tmin, tmax = [float(x.replace(',', '.')) for x in arow[12:14]]
+        rate = f"user_rscale * {ka:.2e} * exp(-{kc:.2f} * user_Auv_star / user_AuvAv)"
+        if not "CWLeo" in arow[17]:
+            rate = f"user_Gstar * {rate}"
+        body += f"{count},{','.join(rr)},{','.join(pp)},{tmin:.2e},{tmax:.2e},{rate}\n"
+        count += 1
+
+if AP:
+    for row in open(f"AP_{AP}K.rates"):
+        srow = row.strip()
+        if srow == "" or srow.startswith("#"): continue
+        arow = srow.split(":")
+        rtype = arow[1]
+        rr = arow[2:4]
+        pp = arow[4:8]
+        ka, kb, kc = [float(x.replace(',', '.')) for x in arow[9:12]]
+        tmin, tmax = [float(x.replace(',', '.')) for x in arow[12:14]]
+        rate = f"user_rscale * {ka:.2e} * exp(-{kc:.2f} * user_Auv_comp / user_AuvAv)"
+        if "CWLeo" in arow[17]:
+            rate = f"user_rbinscale * {rate}"
+        elif "Millar 2018" in arow[17]:
+            if AP == 4000:
+                rate = f"user_rbinscale * {rate}"
+            elif AP == 10000:
+                rate = f"user_Gcomp * {rate}"
+        else:
+            rate = f"user_Gcomp * {rate}"
+        body += f"{count},{','.join(rr)},{','.join(pp)},{tmin:.2e},{tmax:.2e},{rate}\n"
+        count += 1
 
 body = body.replace(",e-,", ",E,")
 
