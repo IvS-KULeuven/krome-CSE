669f50fa380ea67079b46cefca9bc5c69d6caf1d
Mats Esseldeurs <matsesseldeurs@yahoo.com>
2026-09-08 11:29:00 +0200

    cleanup

--- CSE_run_krome.f
+++ CSE_run_krome.f
@@ -1,141 +1,67 @@
-C
-C made by rate version 1.5 on Tue, May 15 2012 at 13:24:22
-C
-c issues users need to be aware of:
-c ---------------------------------
-c  expects species file 'dc.specs' with initial abundances filled in
-c  produces steady state abundance file 'rate13steady.state'
-c  produces full output file 'dc.out'
-c  abundances in the output files are relative to H2 
-c  except that of H2 itself.
-c
-c  this program needs to be compiled and linked to the odes file odes.f
-c  and also to the GEAR package LSODE.
-c
-c  for more info see http://www.udfa.net
-c
       PROGRAM MAIN
       use krome_main 
       use krome_user
       IMPLICIT NONE
 
-      CHARACTER*500 FSPECS,FOUTF,FPARENTS,INFILE,DUMMY
-      CHARACTER*500 DIR, kfile, IDX
-      INTEGER OU, N, NR
-      INTEGER I,J,FSPEC,UPARENTS, UIN
-      DOUBLE PRECISION TSTART,Y(krome_nmols),T,SH,
-     *     X(10),GR,DN,TFINAL,ACCR,HNR,PI,KB,MH,MU,
-     *     ZETA, A_G, X_G, AUV, ALBEDO, RAD, TEMP,
-     *     test_Av, test_xi, test_alb, AuvAv
-      DOUBLE PRECISION, DIMENSION(krome_nrea) :: k
-c      COMMON/BL1/ X,GR,DN,ACCR,HNR
-c      COMMON/BL10/ SH
-c      COMMON/BL3/ Y,X_G,A_G,TEMP,AV,ZETA,ALBEDO,RAD
-C  NC = Number of conserved species, NR = Number of reactions, N = Number of species (ODEs)
-      PARAMETER(OU=8,NR=6173,N=krome_nmols)
+      CHARACTER*500 FOUTF,FPARENTS,INFILE,DUMMY
+      CHARACTER*500 DIR, IDX
+      INTEGER j,UPARENTS, UIN
+      DOUBLE PRECISION dt,Y(krome_nmols),
+     *     DN,TFINAL,PI,
+     *     AUV, ALBEDO, RAD, TEMP,
+     *     AuvAv
 
-C  PHYSICAL CONSTANTS
-      DATA PI,MH,MU,KB/3.1415927,1.6605E-24,2.2,1.3807E-16/
-C
-C      write(*,*) '--------------------------------'
-      write(*,*) ' >> CSE_run_krome is running ...'
-C      write(*,*) '--------------------------------'
+      ! PHYSICAL CONSTANTS
+      DATA PI/3.1415927/
 
+      print*,  ' >> CSE_run_krome is running ...'
 
-C  SPECIES FILE NAME
-      FSPECS = 'rate16_krome.specs' 
-C  OUTPUT FILE NAME
-c      FOUTF = 'fortrandc13-C-rho5.63E+06-T2500-d1-Av1.out' 
-      
-c      FPARENTS = 'rates/C.parents'
-C
       UPARENTS = 37
       UIN = 16
 
-C  ---> READ IN INPUT PARAMETERS
-CCCCCCCCCCCCCCCCCCCCCCCCCCCC
+      ! ---> READ IN INPUT PARAMETERS
 
       CALL GETARG(1,INFILE)
-
       INFILE = TRIM(INFILE)
-c      write(*,*) 'Input file:', INFILE
-
       IF(INFILE.EQ.'') THEN
-          WRITE(*,*) 'ERROR: No input file specified!'
-          WRITE(*,*) 'Useage: % ./run_CSE_krome input_parameters.in'
+          print*,  'ERROR: No input file specified!'
+          print*,  'Useage: % ./run_CSE_krome input_parameters.in'
           STOP
       END IF
 
       OPEN(UNIT=UIN,FILE=INFILE)
-C      DO I=1,4
-C         READ(UIN,*)
-C      END DO
+
       READ(UIN,*) DUMMY,DUMMY,DN
       READ(UIN,*) DUMMY,DUMMY,TEMP
       READ(UIN,*) DUMMY,DUMMY,RAD
       READ(UIN,*) DUMMY,DUMMY,AUV
       READ(UIN,*) DUMMY,DUMMY,TFINAL
-      READ(UIN,*) DUMMY,DUMMY,TSTART
+      READ(UIN,*) DUMMY,DUMMY,dt
       READ(UIN,*) DUMMY,DUMMY,FPARENTS
       READ(UIN,*) DUMMY,DUMMY,FOUTF
       READ(UIN,*) DUMMY,DUMMY,DIR
       READ(UIN,*) DUMMY,DUMMY,IDX
       CLOSE(UNIT=UIN)
 
-     
-c      
-C  open parent species file
-      OPEN(UNIT=UPARENTS, FILE=FPARENTS, STATUS='OLD')
-
-C
-C  Physical parameters - temperature, density, cosmic ray
-c  ionisation and UV radiation field scaling factors and visual extinction
-
-      ZETA = 1.0
-C  GRAIN RADIUS (cm) (0.1 micron = 1.0E-5 cm)
-      A_G = 1.0E-5
-C  GRAIN NUMBER DENSITY/H2 (assuming gas/dust = 200, rho = 3.5 g/cm^3)
-C  USED FOR H-ATOM ACCRETION CALCULATION
-      X_G = 1.5E-12
-C	HNR = 1.0 FOR DENSE CLOUD CHEMISTRY
-      HNR = 1.0
-C   GRAIN ALBEDO
       ALBEDO = 0.5
-C SET STICKING COEFFICIENT FOR H ATOMS
-      SH = 0.3
-
       AuvAv = 4.65
-C
-      ACCR = SH*PI*(A_G**2.0)*DN*X_G*(8.0*KB*TEMP/(PI*MH))**0.5
-C  H-ATOM ACCRETION RATE
-C SET GRAIN SURFACE FORMATION OF H2 TO CLASSICAL RATE
-C     GR = 5.2E-17*(TEMP*3.33E-3)**0.5
-C     ACCR = GR*DN
 
-C      WRITE(*,*)'--------------------------------'
-      WRITE(*,*)'Input parameters:'
-      WRITE(*,*)'       dens   ',DN
-      WRITE(*,*)'       temp   ',TEMP
-      WRITE(*,*)'       RAD    ',RAD
-      WRITE(*,*)'       Auv    ',AUV
-      WRITE(*,*)'       time   ',TSTART
-C      WRITE(*,*)'--------------------------------'
-C
-c Input section
-c -------------
-C read species file.. get species names, masses and initial abundances
-C
-C  parent species tov H2
-C  density #/cm3
-      DO 2 I = 1,N
-c    print *, 'Reading species:', I
-         READ(UPARENTS,100) Y(I)
-c        write(*,*) Y(I)
-         IF(Y(I).GT.0) THEN
-            Y(I)=Y(I) 
-c          write(*,*) Y(I)
-         ENDIF
- 2    CONTINUE
+      print*, '--------------------------------'
+      print*, 'Input parameters:'
+      print*, '       dens   ',DN
+      print*, '       temp   ',TEMP
+      print*, '       RAD    ',RAD
+      print*, '       Auv    ',AUV
+      print*, '       time   ',dt
+      print*, '--------------------------------'
+
+      ! open parent species file
+      OPEN(UNIT=UPARENTS, FILE=FPARENTS, STATUS='OLD')
+      ! parent species tov H2
+      ! density #/cm3
+      do j = 1,krome_nmols
+         read(UPARENTS,100) Y(j)
+      enddo
 
       call krome_init()
 
@@ -144,43 +70,16 @@ c          write(*,*) Y(I)
       call krome_set_user_alb(ALBEDO)
       call krome_set_user_AuvAv(AuvAv)
 
-c     Normalise abudances and balance charge conservation with e-
-c      call krome_consistent_x(Y)
-
-c     Test if values are correct in KROME
-c
-c      test_Av =  krome_get_user_av()
-c      test_xi =  krome_get_user_xi()
-c      test_alb =  krome_get_user_alb()
-c      WRITE(*,*) 'xi  ',test_xi
-c      WRITE(*,*) 'Av  ',test_Av
-c      WRITE(*,*) 'abl ',test_alb
-c
-      kfile = DIR 
-c      write(*,*) 'kfile: ', kfile
-
-      k = krome_get_coef(TEMP,Y)
-      OPEN(UNIT=223, FILE=kfile, STATUS = 'REPLACE')
-      do i=1, size(k)
-          write(223, 101) i, k(i)
-      enddo
-      CLOSE(UNIT=223)
-
-      ! DN = (DN * (1.0 + 4.0*0.085) * 1.6605E-24  ) 
-c     ! DN = HNR * mu * mH   w.r.t. Htot
-
       Y = Y*DN
-      call krome(Y,  TEMP, TSTART)
+      call krome(Y,  TEMP, dt)
       Y = Y/DN
-      
 
       OPEN(UNIT=222, FILE=FOUTF, STATUS = 'REPLACE')
-      do i=1, size(Y)
-          write(222, 100) Y(i)
+      do j=1, size(Y)
+          write(222, 100) Y(j)
       enddo
       CLOSE(UNIT=222)
 
  100  FORMAT(20X,ES11.2E3)
- 101  FORMAT(20X,I4,ES11.2E3)
       
       end program main
--- build_UMIST.sh
+++ build_UMIST.sh
@@ -10,7 +10,7 @@ extra_args="$@"
 # This script is used to build the krome code compatible with the UMIST rate16 and rate22 chemical network.
 
 # Copy the UMIST rate16 network to the krome tools directory
-cp -r umist2krome_custm.py krome/tools/
+# cp -r umist2krome_custm.py krome/tools/
 
 if [ -z "$arg" ]
 then
@@ -18,60 +18,62 @@ then
     echo "Please specify the network." 
     echo "Example: ./build_UMIST.sh umist_rate16"
     echo "Example: ./build_UMIST.sh umist_rate22"
-    echo "You can also add options: ./build_UMIST.sh umist_rate16 -IP"
-    echo "Or: ./build_UMIST.sh umist_rate22 -AP"
+    echo "You can also add options:"
+    echo "-AP=<value>  Activating accretion photons for a companion with <value> K."
+    echo "             Options are 4000 K, 6000 K and 10000 K."
+    echo "-IP          Activating internal photons from the AGB star."
     echo "---------------------------------------------"
     exit 1
 
 fi
 
-if [ $arg == 'umist_rate16' ]; then
-    echo "umist rate16 network selected."
-    cp -r umist_rate16.rates krome/tools/
-elif [ $arg == 'umist_rate22' ]; then
-    echo "umist rate22 network selected."
-    cp -r ../rate22_cse_code/rate22_final.rates krome/tools/umist_rate22.rates
-    if [ -z "$extra_args" ]; then
-        echo "No extra arguments provided."
-    else
-        echo "Extra arguments provided: $extra_args"
-        if [[ "$extra_args" == *"-AP"* ]]; then
-            ap_value=$(echo "$extra_args" | grep -oP '(?<=-AP=)[^ ]*')
-            echo "AP option detected. Activating accretion photons for a companion with $ap_value K."
-            [ -n "$ap_value" ] && cp -r ../rate22_cse_code/AP_"$ap_value"K.rates krome/tools/
-        fi
-        if [[ "$extra_args" == *"-IP"* ]]; then
-            echo "IP option detected. Deactivating all photoreactions."
-            cp -r ../rate22_cse_code/IP.rates krome/tools/
-        fi
-    fi
-else
-    echo $arg "is not a present chemical network."
-    exit 1
-fi
+# if [ $arg == 'umist_rate16' ]; then
+#     echo "umist rate16 network selected."
+#     cp -r umist_rate16.rates krome/tools/
+# elif [ $arg == 'umist_rate22' ]; then
+#     echo "umist rate22 network selected."
+#     cp -r ../rate22_cse_code/rate22_final.rates krome/tools/umist_rate22.rates
+#     if [ -z "$extra_args" ]; then
+#         echo "No extra arguments provided."
+#     else
+#         echo "Extra arguments provided: $extra_args"
+#         if [[ "$extra_args" == *"-AP"* ]]; then
+#             ap_value=$(echo "$extra_args" | grep -oP '(?<=-AP=)[^ ]*')
+#             echo "AP option detected. Activating accretion photons for a companion with $ap_value K."
+#             [ -n "$ap_value" ] && cp -r ../rate22_cse_code/AP_"$ap_value"K.rates krome/tools/
+#         fi
+#         if [[ "$extra_args" == *"-IP"* ]]; then
+#             echo "IP option detected. Activating internal photons from the AGB star."
+#             cp -r ../rate22_cse_code/IP.rates krome/tools/
+#         fi
+#     fi
+# else
+#     echo $arg "is not a present chemical network."
+#     exit 1
+# fi
 
 # Navigate to correct krome directory
-cd krome/tools
+cd umist_network
 
 # Build the UMIST rate16 network in KROME format
 pwd
-echo ">> Building the UMIST rate16 network in KROME format."
-python umist2krome_custm.py $arg $extra_args
+echo ">> Building the UMIST $arg network in KROME format."
+python umist2krome.py $arg $extra_args
 
 # Add shielding to the H2 reaction (deactivate the H2 photodissociation reaction)
 sed -i '/,H2,,H,H,,,.*user_xi/s/$/ * 0/' network_umist.dat
 
 # Copy the network to the networks directory
-cp -r network_umist.dat ../networks/
+cp -r network_umist.dat ../krome/networks/
 
-echo "UMIST rate16 network has been converted to the correct format."
-echo "KROME will now be built with the UMIST rate16 network."
+echo "UMIST $arg network has been converted to the correct format."
+echo "KROME will now be built with the UMIST $arg network."
 echo "---------------------------------------------"
 echo ""
 
 
 # Load the krome module
-cd ../
+cd ../krome
 # Pass extra_args to krome if present
 python krome -n networks/network_umist.dat -iRHS -noSinkCheck -noRecCheck -noTlimits -unsafe -skipODEthermo -skipJacobian
 # -shielding R14
